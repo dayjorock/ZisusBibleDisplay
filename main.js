ecc3309aa6628f6bcc60a06c79a223a7b99c9f39
dayjorock <dayjoofficial@gmail.com>
2026-09-09 16:56:16 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,67 @@
+const { app, BrowserWindow, screen } = require('electron');
+
+let controllerWindow;
+let displayWindow;
+
+const GOOGLE_WEB_APP_BASE_URL = 'https://script.google.com/macros/s/AKfycbyDdykO1vcXWIBGajQoPzgk0_60XB8ThEYa6JJaOoI-p2BEKNfDnS4buWXYTS1Dnzxf/exec';
+
+function launchSoftwareStudio() {
+  const displays = screen.getAllDisplays();
+  let mainDisplay = screen.getPrimaryDisplay();
+  let projectorDisplay = displays.find(d => d.bounds.x !== 0 || d.bounds.y !== 0) || mainDisplay;
+
+  controllerWindow = new BrowserWindow({
+    width: 1100,
+    height: 750,
+    x: mainDisplay.bounds.x + 50,
+    y: mainDisplay.bounds.y + 50,
+    title: "Zisus Bible Display - Controller Deck",
+    backgroundColor: '#1a1a1a',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      backgroundThrottling: false
+    }
+  });
+
+  controllerWindow.webContents.session.setPermissionCheckHandler((webContents, permission) => {
+    if (permission === 'media') return true;
+    return false;
+  });
+  controllerWindow.webContents.session.setPermissionRequestHandler((webContents, permission, callback) => {
+    if (permission === 'media') return callback(true);
+    return callback(false);
+  });
+
+  controllerWindow.loadURL(`https://script.google.com/macros/s/AKfycbyDdykO1vcXWIBGajQoPzgk0_60XB8ThEYa6JJaOoI-p2BEKNfDnS4buWXYTS1Dnzxf/exec?view=controller`);
+
+  displayWindow = new BrowserWindow({
+    x: projectorDisplay.bounds.x,
+    y: projectorDisplay.bounds.y,
+    width: projectorDisplay.bounds.width,
+    height: projectorDisplay.bounds.height,
+    fullscreen: true,
+    frame: false,
+    alwaysOnTop: false,
+    title: "Zisus Bible Display - Live Broadcast Canvas",
+    backgroundColor: '#000000',
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  });
+
+  displayWindow.loadURL(`https://script.google.com/macros/s/AKfycbyDdykO1vcXWIBGajQoPzgk0_60XB8ThEYa6JJaOoI-p2BEKNfDnS4buWXYTS1Dnzxf/exec?view=display`);
+
+  controllerWindow.on('closed', () => { if (process.platform !== 'darwin') app.quit(); });
+  displayWindow.on('closed', () => { displayWindow = null; });
+}
+
+app.commandLine.appendSwitch('use-fake-ui-for-media-stream');
+
+app.whenReady().then(() => {
+  setTimeout(launchSoftwareStudio, 1000);
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
